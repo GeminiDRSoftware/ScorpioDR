a112bc32f7d560a8f38d0339ac6f43cfffd0843f
chris-simpson <csimpson@gemini.edu>
2020-09-11 15:41:39 -1000

    ScorpioCCD version of trimOverscan()

--- scorpiodr/scorpio/primitives_scorpio_ccd.py
+++ scorpiodr/scorpio/primitives_scorpio_ccd.py
@@ -28,6 +28,42 @@ class ScorpioCCD(Scorpio, CCD):
         self.inst_lookups = 'scorpiodr.scorpio.lookups'
         self._param_update(parameters_scorpio_ccd)
 
+    def trimOverscan(self, adinputs=None, suffix=None):
+        """
+        The trimOverscan primitive trims the overscan region from the input
+        AstroData object and updates the headers.
+
+        Parameters
+        ----------
+        suffix: str
+            suffix to be added to output files
+        """
+        log = self.log
+        log.debug(gt.log_message("primitive", self.myself(), "starting"))
+        timestamp_key = self.timestamp_keys[self.myself()]
+
+        for ad in adinputs:
+            if ad.phu.get(timestamp_key) is not None:
+                log.warning('No changes will be made to {}, since it has '
+                            'already been processed by trimOverscan'.
+                            format(ad.filename))
+                continue
+
+            ad = gt.trim_to_data_section(ad,
+                                    keyword_comments=self.keyword_comments)
+            # Tidy up additional header keywords that are too complicated
+            # for gt.trim_to_data_section() to handle
+            del ad.hdr['OVRSECS*']
+            del ad.hdr['OVRSECP*']
+            kw = ad._keyword_for('data_section')
+            del ad.hdr[f'{kw}?*']
+
+            # Set keyword, timestamp, and update filename
+            ad.phu.set('TRIMMED', 'yes', self.keyword_comments['TRIMMED'])
+            gt.mark_history(ad, primname=self.myself(), keyword=timestamp_key)
+            ad.update_filename(suffix=suffix, strip=True)
+        return adinputs
+
     def myNewPrimitive(self, adinputs=None, **params):
         """
         Description...
