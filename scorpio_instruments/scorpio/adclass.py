441271ad74586ceb2018b42e750359103828d1b9
Landon Gelman <lgelman2@illinois.edu>
2021-01-11 11:40:31 -0600

    Removed old testing code.

--- scorpio_instruments/scorpio/adclass.py
+++ scorpio_instruments/scorpio/adclass.py
@@ -53,14 +53,6 @@ class AstroDataScorpio(AstroDataGemini):
         if self.phu.get('OBSTYPE') == 'BIAS':
             return TagSet(['BIAS', 'CAL', 'CCD'], blocks=['IMAGE', 'SPECT'])
 
-    # More tags needs to be added by the Scorpio DR team
-    # At this time, Gemini DR expects the following tags to be implemented.
-    #    IMAGING, LS (for longslit), BUNDLE, FLAT, TWILIGHT, GCALFLAT.
-    #    All type of flats must also be CAL and FLAT.
-    #    CCD, NIR
-    # Also probably needed:
-    #    NODANDSHUFFLE, HIFREQ (High time resolution)
-
     @astro_data_tag
     def _tag_flat(self):
         if self.phu.get('OBSTYPE') == 'FLAT':
@@ -79,7 +71,6 @@ class AstroDataScorpio(AstroDataGemini):
             return TagSet(['NIR'], blocks=['CCD'])
 
     # TODO: Check the work I did in ScorpioDR_old for GCAL tags
-
     @astro_data_tag
     def _flat_type(self):
         bun = self.phu.get('BUNDLE')
@@ -90,20 +81,13 @@ class AstroDataScorpio(AstroDataGemini):
         if bun == 'F' and obj == 'GCALFLAT' and shut == 'CLOSED':
             return TagSet(['LAMPOFF', 'NIR'], blocks=['CCD'])
 
-    # Write our own _parse_section based on the method in the Gemini adclass
-    # It should take into consideration the number of amplifiers in Scorpio
-    """
-    def _parse_section(self, keyword, pretty, namps):
-        section_list = []
-        try:
-            value_filter = (str if pretty else section_to_tuple)
-            process_fn = lambda x: (None if x is None else value_filter(x))
-            section = []
-            for _i in range(1,namps+1):
-                section.append(self.hdr.get(keyword+'{}'.format(_i)))
-            if self.is_single:
-                return
-    """
+    # More tags needs to be added by the Scorpio DR team
+    # At this time, Gemini DR expects the following tags to be implemented.
+    #    IMAGING, LS (for longslit), BUNDLE, FLAT, TWILIGHT, GCALFLAT.
+    #    All type of flats must also be CAL and FLAT.
+    #    CCD, NIR
+    # Also probably needed:
+    #    NODANDSHUFFLE, HIFREQ (High time resolution)
 
     # ----------------------
     # Descriptor definitions
@@ -124,108 +108,6 @@ class AstroDataScorpio(AstroDataGemini):
 
     # For a list of the expected descriptors see the appendix in the
     # Astrodata User Manual.
-
-
-    # Modified 2020-08-27
-    @astro_data_descriptor
-    def amplifier_layout(self):
-        """
-        Returns a tuple of the multi-amplifier shape/layout when multiple 
-        amplifiers exist on one extension. If called on more than one extension,
-        the tuples are returned in a list. Otherwise the shape/layout is 
-        returned as a tuple.
-
-        Returns
-        -------
-        tuple of int / list of tuples of ints
-            Shape or layout of multi-amplifier configuration per extension.
-        """
-        channel = self.channel().upper()
-        namps = lookup.amplifier_count.get(channel)
-        
-        arrsec = self.array_section(pretty=False)
-
-        if self.is_single:
-            v_amps = 0
-            h_amps = 0
-            v_max = 0
-            h_max = 0
-            for arr in arrsec:
-                if arr[1] > h_max:
-                    h_amps += 1
-                    h_max = arr[1]
-                if arr[3] > v_max:
-                    v_amps += 1
-                    v_max = arr[3]
-            amp_shape = (v_amps,h_amps)
-            return amp_shape
-        else:
-            amp_shape = []
-            for sec in arrsec:
-                v_amps = 0
-                h_amps = 0
-                v_max = 0
-                h_max = 0
-                for arr in sec:
-                    if arr[1] > h_max:
-                        h_amps += 1
-                        h_max = arr[1]
-                    if arr[3] > v_max:
-                        v_amps += 1
-                        v_max = arr[3]
-                amp_shape.append((v_amps,h_amps))
-            return amp_shape
-
-    @astro_data_descriptor
-    def amp_total_area(self, pretty=False):
-        """
-        Returns the total array size, including data and overscan for each 
-        amplifier in each extension.
-
-        Returns
-        -------
-        list/(tuple or string)
-            the total amp areas
-        """
-
-        # Get the channel and number of amps
-        #channel, namps = self._get_channel_amp_count()
-        channel = self.channel().upper()
-        namps = lookup.amplifier_count.get(channel)
-
-        # Create a list for the sections
-        read_arrays = []
-
-        # Loop over the namps to get all keywords
-        for amp in range(1,namps+1):
-            # Get values for keywords DATSECn, OVRSECSn, and OVRSECPn
-            datsec = self._parse_section('DATSEC{}'.format(amp), pretty=False)
-            ovrsecs = self._parse_section('OVRSECS{}'.format(amp), pretty=False)
-            ovrsecp = self._parse_section('OVRSECP{}'.format(amp), pretty=False)
-
-            # Concatenate values to new strings like [x1:x2,y1:y2]
-            if self.is_single:
-                x1 = np.min([datsec[0], ovrsecs[0], ovrsecp[0]])
-                x2 = np.max([datsec[1], ovrsecs[1], ovrsecp[1]])
-                y1 = np.min([datsec[2], ovrsecs[2], ovrsecp[2]])
-                y2 = np.max([datsec[3], ovrsecs[3], ovrsecp[3]])
-            else:
-                x1 = np.min([datsec[0][0], ovrsecs[0][0], ovrsecp[0][0]])
-                x2 = np.max([datsec[0][1], ovrsecs[0][1], ovrsecp[0][1]])
-                y1 = np.min([datsec[0][2], ovrsecs[0][2], ovrsecp[0][2]])
-                y2 = np.max([datsec[0][3], ovrsecs[0][3], ovrsecp[0][3]])
-
-            amparr = "[{0}:{1},{2}:{3}]".format(x1+1, x2, y1+1, y2)
-            
-            value_filter = (str if pretty else section_to_tuple)
-            process_fn = lambda x: (None if x is None else value_filter(x))
-
-            read_arrays.append(process_fn(amparr))
-
-        if self.is_single:
-            return read_arrays
-        else:
-            return [read_arrays]
 
     @astro_data_descriptor
     def array_name(self):
@@ -257,34 +139,6 @@ class AstroDataScorpio(AstroDataGemini):
             return name_list
         else:
             return [name_list]
-
-        '''
-        names = []
-
-        for amp in range(1,namps+1):
-            name = self.hdr.get('ARRNAM{}'.format(amp))
-            names.append(name)
-
-        if self.is_single:
-            return names
-        else:
-            return [names]
-        '''
-        '''
-        names = []
-        if self.is_single:
-            for amp in range(1,namps+1):
-                name = self.hdr.get('ARRNAM{}'.format(amp))
-                names.append(name)
-        else:
-            for ext in range(len(self)):
-                extnames = []
-                for amp in range(1,namps+1):
-                    name = self[ext].hdr.get('ARRNAM{}'.format(amp))
-                    extnames.append(name)
-                names.append(extnames)
-        return names
-        '''
 
     @astro_data_descriptor
     def array_section(self, pretty=False):
@@ -422,63 +276,6 @@ class AstroDataScorpio(AstroDataGemini):
         else:
             return [gain_list]
 
-        """
-        gain_list = []
-        if self.is_single:
-            for i in range(namps):
-                try:
-                    gain = self.hdr.get('GAIN{}'.format(i+1))
-                except Exception:
-                    break
-                else:
-                    gain_list.append(gain)
-        else:
-            for i in range(len(self)):
-                ext_gain_list = []
-                for j in range(namps):
-                    try:
-                        gain = self[i].hdr.get('GAIN{}'.format(j+1))
-                    except Exception:
-                        break
-                    else:
-                        ext_gain_list.append(gain)
-                gain_list.append(ext_gain_list)
-        
-        return gain_list
-        """
-
-    @astro_data_descriptor
-    def map_amplifier_to_layout(self, keyword):
-        """
-        Returns the amplifier-specific values (keyword based) in the 
-        shape of the amplifier order (described in lookup.py) on the CCD 
-        detector.
-
-        Returns
-        -------
-
-        """
-        channel = self.channel().upper()
-        layout = self.amplifier_layout()
-        order = lookup.amplifier_order.get(channel)
-        
-        amparr = []
-        for amp in order:
-            kw = self.hdr.get(keyword+'{}'.format(amp))
-            if self.is_single:
-                amparr.append(kw)
-            else:
-                amparr.append(kw[0])
-        
-        amparr = np.array([amparr])
-
-        if self.is_single:
-            amparr = amparr.reshape(layout)
-            return amparr
-        else:
-            amparr = amparr.reshape(layout[0])
-            return [amparr]
-
     @astro_data_descriptor
     def read_noise(self):
         """
@@ -508,43 +305,6 @@ class AstroDataScorpio(AstroDataGemini):
         else:
             return [readnoise_list]
         
-        """
-        rn_list = []
-        if self.is_single:
-            for i in range(namps):
-                try:
-                    rn = self.hdr.get('RDNOIS{}'.format(i+1))
-                except Exception:
-                    break
-                else:
-                    rn_list.append(rn)
-        else:
-            for i in range(len(self)):
-                ext_rn_list = []
-                for j in range(namps):
-                    try:
-                        rn = self[i].hdr.get('RDNOIS{}'.format(j+1))
-                    except Exception:
-                        break
-                    else:
-                        ext_rn_list.append(rn)
-                rn_list.append(ext_rn_list)
-        
-        return rn_list
-        """
-
-    '''
-    def _get_channel_amp_count(self):
-        channel = self.channel().upper()
-        namps = None
-
-        if channel in ['G','R','I','Z']:
-            namps = 4
-        if channel in ['Y','J','H','K']:
-            namps = 32
-
-        return channel, namps
-    '''
     '''
     @astro_data_descriptor
     def gain(self):
