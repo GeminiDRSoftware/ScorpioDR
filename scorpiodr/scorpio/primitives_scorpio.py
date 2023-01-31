770ffe936bb6435a6a9922faefea148dd42a6428
Landon Gelman <28926973+lgelman2@users.noreply.github.com>
2023-01-31 12:35:00 -0600

    Add standardizeInstrumentHeaders based on GMOS.

--- scorpiodr/scorpio/primitives_scorpio.py
+++ scorpiodr/scorpio/primitives_scorpio.py
@@ -5,6 +5,7 @@
 # ------------------------------------------------------------------------------
 
 from geminidr.gemini.primitives_gemini import Gemini
+from gempy.gemini import gemini_tools as gt
 
 from . import parameters_scorpio
 
@@ -38,3 +39,47 @@ class Scorpio(Gemini):
         # this needs to be verified. This would be for the debundled data
         # and inherited and used by the ScorpioImage and ScorpioSpect class.
         return len(ad) in [1]
+
+    def standardizeInstrumentHeaders(self, adinputs=None, suffix=None):
+        """
+        This primitive is used to make the changes and additions to the keywords in the headers of SCORPIO data, specifically.
+
+        2023-01-30 - Landon Gelman
+        The contents of this function are currently intended just for testing SCORPIO's implementation with DRAGONS' core primitives.
+
+        Parameters
+        ----------
+        suffix: str
+            suffix to be added to output files
+        """
+
+        # Instantiate the log
+        log = self.log
+        log.debug(gt.log_message("primitive", self.myself(), "starting"))
+        timestamp_key = self.timestamp_keys[self.myself()]
+
+        for ad in adinputs:
+            if ad.phu.get(timestamp_key):
+                log.warning("No changes will be made to {}, since it has "
+                            "already  been processed by "
+                            "standardizeInstrumentHeaders".format(ad.filename))
+                continue
+
+            # Standardize the headers of the input AstroData object. Update the 
+            # keywords in the headers that are specific to SCORPIO.
+            log.status("Updating keywords that are specific to SCORPIO")
+
+            for desc in ('saturation_level', 'non_linear_level'):
+                kw = ad._keyword_for(desc)
+                comment = self.keyword_comments[kw]
+                dv = getattr(ad, desc)()
+                if isinstance(dv, list):
+                    for ext, value in zip(ad, dv):
+                        ext.hdr.set(kw, value, comment)
+                else:
+                    ad.hdr.set(kw, dv, comment)
+
+            # Timestamp and update filename
+            gt.mark_history(ad, primname=self.myself(), keyword=timestamp_key)
+            ad.update_filename(suffix, strip=True)
+        return adinputs
