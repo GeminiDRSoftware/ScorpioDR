f6650b7899a0e723ecfb4cf9c04c2d4ac8953f10
James E.H. Turner <jturner@gemini.edu>
2026-08-26 16:04:56 -0400

    Provisionally reduce default fitting order for stds until we have higher-res references available.

--- scorpiodr/scorpio/parameters_scorpio_spect.py
+++ scorpiodr/scorpio/parameters_scorpio_spect.py
@@ -1,5 +1,9 @@
 # This parameter file contains the parameters related to the primitives
 # defined in the primitives_scorpio_spect.py file.
 
+from geminidr.core import parameters_spect
 from gempy.library import config
+
+class calculateSensitivityConfig(parameters_spect.calculateSensitivityConfig):
+    order = config.RangeField("Order of fitting function", int, 4, min=1)
 
